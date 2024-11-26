4e1a22cdae1646256518f8fab58055600e929f29
“DesaraB” <desarabaroti99@gmail.com>
2024-11-26 11:35:39 +0000

    changes

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
+int (*find_print(char format_char, specifier_t specifiers[]))(va_list)
 {
 	unsigned int i = 0;
 
@@ -10,9 +10,30 @@ int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
 	{
 		if (specifiers[i].specifier[0] == format_char)
 		{
-			return (specifiers[i].handle);
+			return (specifiers[i].print);
 		}
 		i++;
 	}
 	return (NULL);
+}
+
+int process_format(const char *format, va_list args, specifier_t specifiers[])
+{
+	return (0);
+}
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int len;
+
+	specifier_t specifiers[] = {
+		{"c", handle_char},
+		{"s", handle_string},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+	len = process_format(format, args, specifiers);
+	va_end(args);
 }
